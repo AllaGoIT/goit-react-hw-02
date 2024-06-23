69b349a3563c5384c6ad5ec9020072ebef43166d
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-06-23 23:52:47 +0300

    style

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,8 +1,9 @@
-import name from "../name.json"
+import name from "../name.json";
+import css from "./Options.module.css"
 const Options = ({onclick, onUpdate, total}) => {
 
     return (
-        <div>
+        <div className={css.container}>
             <button name={name.good} onClick={() => {onclick("good")}}>Good</button>
             <button name={name.neutral}onClick={() => {onclick("neutral")}}>Neutral</button>
             <button name={name.bad} onClick={() => { onclick("bad") }}>Bad</button>
